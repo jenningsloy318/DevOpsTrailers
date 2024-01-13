83f2e392c5bd873fcc3f6b8eb4e890d122fb8ba5
Jennings Liu <jenningsloy318@gmail.com>
2024-01-13 18:55:06 +0800

    fix: add proxy

--- Crawlers/crawler_hidden_brain.py
+++ Crawlers/crawler_hidden_brain.py
@@ -1,7 +1,3 @@
-import requests
-#write crawler to scripe hidden brain podcast audio and transcript, save it locally, from https://hiddenbrain.org/, use class
-
-
 import bs4
 import requests
 import os
@@ -9,7 +5,11 @@ from lxml import html
 import time
 for n in range(1,10):
     url = f'https://hiddenbrain.org/category/podcast/page/{n}/'
-    response = requests.get(url,allow_redirects=True)
+    proxies = {
+        'http': 'socks5://192.168.8.1:38080',
+        'https': 'socks5://192.168.8.1:38080',
+    }
+    response = requests.get(url, allow_redirects=True, proxies=proxies)
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     articales= soup.find_all('article',attrs={'itemscope':'itemscope',})
     os.makedirs('hidden_brain',exist_ok=True)
@@ -17,7 +17,7 @@ for n in range(1,10):
         episode_link = arti.find('a', attrs={'rel':'bookmark'})['href']
         print(f'starting process episode {episode_link}')
         episode_title=episode_link.split('/')[-2]
-        episode_response=requests.get(episode_link)
+        episode_response = requests.get(episode_link, proxies=proxies)
         episode_soup= bs4.BeautifulSoup(episode_response.text,'html.parser')
         #dom = etree.HTML(str(episode_soup))
         dom = html.fromstring(episode_response.content)
@@ -25,7 +25,7 @@ for n in range(1,10):
         audio_link = dom.xpath('/html/body/div[2]/div/div/div[1]/main/article/div/div/figure/audio[last()]/@src')[0]
         audio_file_path = f'hidden_brain/{date}-{episode_title}.mp3'
         if not os.path.exists(audio_file_path) or os.path.getsize(audio_file_path) == 0 :
-            with requests.get(audio_link,stream=True) as r:
+            with requests.get(audio_link, stream=True, proxies=proxies) as r:
                 r.raise_for_status()
                 with open(audio_file_path, 'wb') as audio_file:
                     for chunk in r.iter_content(chunk_size=4096):
